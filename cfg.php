91c02d3ca905e7ce1726b2b1f122a54ce8c29ef5
Khairil2203 <54951604+Khairil2203@users.noreply.github.com>
2019-09-18 16:25:00 +0700

    Create cfg.php

---
+++ cfg.php
@@ -0,0 +1,56 @@
+<?php
+
+
+//list akun 1
+
+$uid = "67529996";
+$n = "ffffffffba4709f20d15ffd50d15ffd5";
+
+//list akun 2
+
+$uid1 = "67530240";
+$n1 = "ffffffffba4709f20d15ffd50d15ffd5";
+
+//list akun 3
+
+$uid2 = "62003581";
+$n2 = "ffffffffba4709f20d15ffd50d15ffd5";
+
+//list akun 4
+
+$uid3 = "61891525";
+$n3 = "ffffffffba4709f20d15ffd50d15ffd5";
+
+//list akun 5
+
+$uid4 = "67540588";
+$n4 = "ffffffffba4709f20d15ffd50d15ffd5";
+
+//list akun 6
+
+$uid5 = "67296615";
+$n5 = "ffffffffd8893d490d15ffd50d15ffd5";
+
+//list akun 7
+
+$uid6 = "67557593";
+$n6 = "ffffffffba4709f20d15ffd50d15ffd5";
+
+//list akun 8
+
+$uid7 = "67557609";
+$n7 = "ffffffffba4709f20d15ffd50d15ffd5";
+
+//list akun 9
+
+$uid8 = "67557621";
+$n8 = "ffffffffba4709f20d15ffd50d15ffd5";
+
+//list akun 10
+
+$uid9 = "67557628";
+$n9 = "ffffffffba4709f20d15ffd50d15ffd5";
+
+
+
+?>
